35a9b91b09d4a44f5ca2dd1145cea2abeae096c8
Ankit <ankitmittal0814@gmail.com>
2023-12-19 22:21:02 +0530

    Update LogoutBtn.jsx

--- MegaBlog/src/components/Header/LogoutBtn.jsx
+++ MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -9,7 +9,14 @@ function LogoutBtn() {
       dispatch(logout());
     });
   };
-  return <button className="">Logout</button>;
+  return (
+    <button
+      className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 roundd-full"
+      onClick={logoutHandler}
+    >
+      Logout
+    </button>
+  );
 }
 
 export default LogoutBtn;
