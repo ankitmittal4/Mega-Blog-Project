0246db0b45c2ded2304bff134d3d4ee90dd1dd85
Ankit <ankitmittal0814@gmail.com>
2023-12-19 09:07:56 +0530

    Update mega project

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,4 +1,21 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
 
-export { Header, Footer };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  PostForm,
+};
