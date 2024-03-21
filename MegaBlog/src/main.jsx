bd7375fbf277efb574e5459ca0f2fade5fea1e7e
Ankit <ankitmittal0814@gmail.com>
2024-03-21 17:41:18 +0530

    Update main.jsx

--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -6,15 +6,9 @@ import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import { AuthLayout, Login } from "./components/index.js";
+import { AuthLayout } from "./components/index.js";
 
-import AddPost from "./pages/AddPost";
 import Signup from "./pages/Signup";
-import EditPost from "./pages/EditPost";
-
-import Post from "./pages/Post";
-
-import AllPosts from "./pages/AllPosts";
 
 const router = createBrowserRouter([
   {
@@ -25,14 +19,14 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
-      {
-        path: "/login",
-        element: (
-          <AuthLayout authentication={false}>
-            <Login />
-          </AuthLayout>
-        ),
-      },
+      // {
+      //   path: "/login",
+      //   element: (
+      //     <AuthLayout authentication={false}>
+      //       <Login />
+      //     </AuthLayout>
+      //   ),
+      // },
       {
         path: "/signup",
         element: (
@@ -40,37 +34,6 @@ const router = createBrowserRouter([
             <Signup />
           </AuthLayout>
         ),
-      },
-      {
-        path: "/all-posts",
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <AllPosts />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/add-post",
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <AddPost />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/edit-post/:slug",
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <EditPost />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/post/:slug",
-        element: <Post />,
       },
     ],
   },
